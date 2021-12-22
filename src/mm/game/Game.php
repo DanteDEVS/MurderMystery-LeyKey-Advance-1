b1835749182a2ea6d69d9cf603382c7f98ef5280
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-12-22 03:02:46 +0200

    Soon! some scoreboard changes

--- src/mm/game/Game.php
+++ src/mm/game/Game.php
@@ -53,6 +53,7 @@ use mm\tasks\{ArrowTask, CollideTask, CooldownTask, DespawnSwordEntity, SpawnGol
 
 class Game implements Listener{
 
+    const PHASE_WAITING = 0;
     const PHASE_LOBBY = 0;
     const PHASE_GAME = 1;
     const PHASE_RESTART = 2;
@@ -188,6 +189,10 @@ class Game implements Listener{
 
                 case Game::PHASE_LOBBY:
                     $this->createScoreboard($player, "§l§eMURDER MYSTERY", $this->plugin->getConfig()->get("LobbyScoreboard"));
+                break;
+			  
+                case Game::PHASE_WAITING:
+                    $this->createScoreboard($player, "§l§eMURDER MYSTERY", $this->plugin->getConfig()->get("WaitingScoreboard"));
                 break;
             }
         }
