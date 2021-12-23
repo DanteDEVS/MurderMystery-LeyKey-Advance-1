d0d21ac11de3497c4a7c4fda995c5d196a4c452c
DEVS <83214438+DanteDEVS@users.noreply.github.com>
2021-12-23 12:48:08 +0200

    test

--- src/mm/MurderMystery.php
+++ src/mm/MurderMystery.php
@@ -10,7 +10,7 @@ use pocketmine\event\player\{PlayerInteractEvent, PlayerChatEvent};
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\entity\Entity;
 
-use mm\utils\{GameChooser, IronSwordEntity, Vector, DeadPlayerEntity};
+use mm\utils\{GameChooser, IronSwordEntity, DiamondSwordEntity, WoodSwordEntity, StoneSwordEntity, GoldSwordEntity, Vector, DeadPlayerEntity};
 use mm\provider\Provider;
 use mm\game\Game;
 
@@ -39,6 +39,10 @@ class MurderMystery extends PluginBase implements Listener{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->provider->loadGames();
         Entity::registerEntity(IronSwordEntity::class, true);
+        Entity::registerEntity(DiamondSwordEntity::class, true);
+        Entity::registerEntity(GoldSwordEntity::class, true);
+        Entity::registerEntity(StoneSwordEntity::class, true);
+        Entity::registerEntity(WoodSwordEntity::class, true);
         Entity::registerEntity(DeadPlayerEntity::class, true, ["DeadPlayerEntity"]);       
         $this->prefix = $this->getConfig()->get("Prefix") . " ";
     }
